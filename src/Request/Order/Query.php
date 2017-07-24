2e97a69633df098804a6d825390b1a7d7b93d3e0
n.yotsov <karboratorr@gmail.com>
2017-07-24 12:12:06 +0300

    - Query properties fix

--- src/Request/Order/Query.php
+++ src/Request/Order/Query.php
@@ -12,7 +12,14 @@ use SslStoreSdk\Core\BaseRequest;
 
 class Query extends BaseRequest
 {
+    public $StartDate;
+    public $EndDate;
+    public $CertificateExpireToDate;
+    public $CertificateExpireFromDate;
+    public $DomainName;
+    public $SubUserID;
     public $ProductCode;
-    public $ProductType;
-    public $NeedSortedList;
+    public $DateTimeCulture;
+    public $PageNumber;
+    public $PageSize;
 }
